0453d63d4d81a2b8effc09b8cda9154a1be449ad
DESKTOP-QK3PL8N\LENOVO <ilchenko.dp@gmail.com>
2022-07-24 23:31:09 +0300

    built, but doesn't work. cleaned the functions and vars

--- trafficator-light-module-c18.X/main.c
+++ trafficator-light-module-c18.X/main.c
@@ -19,19 +19,54 @@
 
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 #include "user.h"          /* User funct/params, such as InitApp */
-#include "user.c"
 
-// BEGIN CONFIG
-/*#pragma config FOSC = INTOSC_EC // Oscillator Selection bits (internal oscillator)
-#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT disabled)
-#pragma config PWRTE = OFF // Power-up Timer Enable bit (PWRT disabled)
-#pragma config BOREN = ON // Brown-out Reset Enable bit (BOR enabled)
-#pragma config LVP = OFF // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
-#pragma config CPD = OFF // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
-#pragma config WRT = OFF // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
-#pragma config CP = OFF // Flash Program Memory Code Protection bit (Code protection off)
-#pragma config MCLRE = OFF*/
-//END CONFIG
+// CONFIG1H
+#pragma config OSC = INTIO2     // Oscillator Selection bits (Internal RC oscillator, port function on RA6 and port function on RA7)
+#pragma config FSCM = ON        // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+#pragma config IESO = ON        // Internal External Switchover bit (Internal External Switchover mode enabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOR = ON         // Brown-out Reset Enable bit (Brown-out Reset enabled)
+// BORV = No Setting
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config MCLRE = OFF      // MCLR Pin Enable bit (RA5 input pin enabled, MCLR disabled)
+
+// CONFIG4L
+#pragma config STVR = ON        // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = ON         // Low-Voltage ICSP Enable bit (Low-Voltage ICSP enabled)
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (00200-000FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (001000-001FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot Block (000000-0001FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (00200-000FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (001000-001FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0001FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (00200-000FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (001000-001FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0001FFh) not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
 
 /******************************************************************************/
 /* User Global Variable Declaration                                           */
@@ -42,18 +77,18 @@
 /******************************************************************************/
 /* Main Program                                                               */
 /******************************************************************************/
-unsigned char wasTurningRight;
-unsigned char wasTurningLeft;  
-unsigned char hazardIsOn;
-//Reverse;
-
+bool wasTurningRight;
+bool wasTurningLeft;  
+bool hazardIsOn;
 
 
 void main()
 {
-    int ADC2_value = 0; 
-    int ADC3_value = 0;
-    int ADC_sum_value = 0;
+    int _49A_in_value = 0;
+    int Reverse_in_value = 0;
+    int R_ch_in_value = 0; 
+    int L_ch_in_value = 0;
+    
     
     //T0IF = 0;   // Clear the interrupt
     
@@ -62,51 +97,46 @@ void main()
     //ANSEL = 0b00010011;                     // 0b00010000 Clear Pin selection bits - AN0-AN3 are digital I/O for now
     //TRISIO = 0b00001011;                    // Gp0, Gp1, Gp3 are always as inputs; Gp5 is always output; Gp2, Gp4 are as outputs for now     
     R_ch_out = 0;
-    
     L_ch_out = 0;
+    _49A_out = 0;
     
-    if (Reverse == 1)
+    InitADC(AN4|AN5);               //--Set up AN2 and AN3 --//
+    _49A_in_value = GetADCValue(AN4);
+    //R_ch_in_value = GetADCValue(AN2);
+    //L_ch_in_value = GetADCValue(AN3);
+    Reverse_in_value = GetADCValue(AN5);
+    
+    
+    if (Reverse_in_value > 155)
     {
      ReversOn();                                 //--Turn reverse lights on--//
     return;
     }
-    //__delay_ms(2);
     else 
-    {
-    InitADC(AN2|AN3);               //--Set up AN2 and AN3 --//
-    ADC2_value = GetADCValue(AN2);
-    ADC3_value = GetADCValue(AN3); 
-    }
+
         
-          if (ADC3_value > 4 && ADC2_value > 4)    //--Turn hazard on--//
+          if (_49A_in_value < 10)    //--Turn hazard on--//
             {
-            Turn_Hazard();    
+            Turn_49A();    
             }
-    else  if (ADC3_value > 5 && ADC2_value < 5)                       //--Turn Relay on if left switch is on--//
-            {
-            TurnLeft();
-            }
-    else  if (ADC2_value > 5 && ADC3_value < 5)                      //--Turn Relay on if right switch is on--//
-            {
-            TurnRight();
-            }
-    else  if (wasTurningRight == 1 && wasTurningLeft == 0 && hazardIsOn == 0)// && Reverse == 0)
+    else  if (wasTurningRight == 1 )
             {
             AddRightBlinks();                     // Add 3 blinks to the right channel if left switch has been off//
             }
-    else  if (wasTurningLeft == 1 && wasTurningRight == 0 && hazardIsOn == 0)// && Reverse == 0)
+    else  if (wasTurningLeft == 1 )
             {
             AddLeftBlinks();                       // Add 3 blinks to the left channel if right switch has been off//
             }
     else    {
             //TRISIO2 = 0; R_ch = 0;
             //TRISIO4 = 0; L_ch = 0;
-            ADC2_value = 0; 
-            ADC3_value = 0;
-            ADC_sum_value = 0;
+            L_ch_in_value = 0; 
+            R_ch_in_value = 0;
+            Reverse_in_value = 0;
+            _49A_in_value = 0;
+            
             wasTurningLeft = 0;
             wasTurningRight = 0;
-            hazardIsOn = 0;
             //StopBip();
             }
 }
